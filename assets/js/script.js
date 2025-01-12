62051db4a8b536bbf1218b40e0c93a34f10c4172
Sheharyar <sheharyar.faisal@yahoo.com>
2025-01-12 23:31:45 +0500

    Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
 'use strict';
 
-
-
 /**
- * navbar toggle
+ * Navbar toggle
  */
-
 const header = document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 
@@ -15,26 +12,20 @@ navToggleBtn.addEventListener("click", function () {
 });
 
 /**
- * toggle the navbar when click any navbar link
+ * Toggle the navbar when clicking any navbar link
  */
-
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
+navbarLinks.forEach(link => {
+  link.addEventListener("click", function () {
     header.classList.toggle("nav-active");
     navToggleBtn.classList.toggle("active");
   });
-}
-
-
-
-
+});
 
 /**
- * back to top & header
+ * Back to top & header
  */
-
 const backTopBtn = document.querySelector("[data-back-to-top]");
 
 window.addEventListener("scroll", function () {
@@ -45,4 +36,20 @@ window.addEventListener("scroll", function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
+});
+
+document.querySelectorAll("[data-toggle='dropdown']").forEach(header => {
+  header.addEventListener("click", function () {
+    const content = this.nextElementSibling;  // Get the dropdown content (next sibling)
+    const btn = this.querySelector(".toggle-btn");  // Get the toggle button
+
+    // Toggle content visibility
+    if (content.hidden) {
+      content.hidden = false;  // Show the content
+      btn.textContent ="Collapse";  // Change button text to minus
+    } else {
+      content.hidden = true;  // Hide the content
+      btn.textContent = "Details";  // Change button text to plus
+    }
+  });
 });
